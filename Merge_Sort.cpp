33c41b57d798c5c87cc9ec79a56dcef169d1fb19
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-01 19:07:38 +0600

    Implement merge sort algorithm

---
+++ Merge_Sort.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void mergeArrays(vector<int>& a, int l, int mid, int r) {
+    int n1 = mid - l + 1;
+    int n2 = r - mid;
+
+    vector<int> L(n1), R(n2);
+    for (int i = 0; i < n1; i++) L[i] = a[l + i];
+    for (int j = 0; j < n2; j++) R[j] = a[mid + 1 + j];
+
+    int i = 0, j = 0, k = l;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) a[k++] = L[i++];   // stable
+        else a[k++] = R[j++];
+    }
+    while (i < n1) a[k++] = L[i++];
+    while (j < n2) a[k++] = R[j++];
+}
+
+void mergeSort(vector<int>& a, int l, int r) {
+    if (l >= r) return;
+    int mid = l + (r - l) / 2;
+    mergeSort(a, l, mid);
+    mergeSort(a, mid + 1, r);
+    mergeArrays(a, l, mid, r);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+
+    mergeSort(a, 0, n - 1);
+
+    for (int i = 0; i < n; i++) {
+        cout << a[i] << (i + 1 == n ? '\n' : ' ');
+    }
+    return 0;
+}
