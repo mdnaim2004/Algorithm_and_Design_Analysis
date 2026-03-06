6ebc9e0bffdce775cc6487155e6fbb3b96fff2e3
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-06 13:06:39 +0600

    ]midterm exam materials in Algorihm and design analysis

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -2,24 +2,23 @@
 using namespace std;
 
 void quick_sort(vector<int>& arr, int l, int h){
-    int i = l;
-    int j = h;
-    int pivot = arr[l];
-
     if(l<h){
-        while(i<j){
-            while(arr[i] <= pivot && i<h)
+        int i = l;
+        int j = h;
+        int pivot = arr[l];
+
+        while(i < j){
+            while(i<h && arr[i] <= pivot){
                 i++;
-
-            while(arr[j] > pivot)
+            }
+            while(arr[j] > pivot){
                 j--;
-            
-            if(i<j)
-                swap(arr[l], arr[j]);
+            }
+            if(i<j){
+                swap(arr[i], arr[j]);
+            }
         }
-        arr[l] = arr[i];
-        arr[j] = pivot;
-
+        swap(arr[l], arr[j]);
         quick_sort(arr, l, j-1);
         quick_sort(arr, j+1, h);
     }
@@ -32,12 +31,13 @@ int main(){
     vector<int> arr(n);
 
     for(int i=0; i<n; i++)
-        cin >> arr[i];
+        cin >>arr[i];
 
     quick_sort(arr, 0, n-1);
 
-    for(int i=0; i<n; i++)
-        cout << arr[i] << " ";
+    for(int x : arr)
+        cout << x <<" ";
+    cout << endl;
 
     return 0;
 }
