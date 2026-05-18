eba1dfdb9efc3b3a19b17cc428708cd8254d02ab
Md Naim <naim.cse2004@gmail.com>
2026-05-18 08:22:14 +0600

    Refactor activity.cpp for improved code formatting and readability

--- activity.cpp
+++ activity.cpp
@@ -4,31 +4,29 @@ using namespace std;
 int main(){
     int n;
     cin >> n;
-    vector<pair<int,int>> v;
 
+    vector<pair<int, int>> v;
+    
     for(int i=0; i<n; i++){
         int start, finish;
-        cin >> start>>finish;
+        cin >> start >> finish;
 
         v.push_back({finish, start});
-
-
     }
-
     sort(v.begin(), v.end());
 
-    
-    int cnt = 1;
     int lastfinish = v[0].first;
+    int cnt = 1;
+
     for(int i=1; i<n; i++){
         int start = v[i].second;
         int finish = v[i].first;
 
-        if(start  >= lastfinish){
+        if(start >= lastfinish){
             cnt++;
-            lastfinish = start;
+            lastfinish = finish;
         }
     }
-    cout <<cnt <<"\n";
+    cout << cnt << "\n";
     return 0;
 }
